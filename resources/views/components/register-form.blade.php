f01dda544773901799d1b5ee1eea72529b4a1685
mina <minaremon1986@gmail.com>
2023-11-02 22:27:41 +0200

    make changes at the create.blade.php register-form.blade.php and home-layout.blade.php to make the register form fit on the width of small amd mideum screens

--- resources/views/components/register-form.blade.php
+++ resources/views/components/register-form.blade.php
@@ -89,12 +89,12 @@
                 class="font-medium">{{$message}}</p>
         @enderror
     </div>
-    <div class="grid place-items-center grid-cols-6 w-full">
+    <div class="grid place-items-start w-full max-sm:grid-cols-1 max-sm:place-items-center">
         <button type="submit"
-                class="grid place-items-start text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                class="grid place-items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Create account
         </button>
-        <div class=" grid place-content-end place-items-end w-full text-sm font-medium text-gray-500 dark:text-gray-300">
+        <div class="grid place-content-center place-items-center w-full text-sm font-medium text-gray-500 dark:text-gray-300 max-sm:pt-2 max-lg:place-content-start max-lg:mt-3">
             <div class="flex"><span>already registered?</span> <a href="{{ route('login.create') }}" class="text-blue-700 hover:underline dark:text-blue-500">login</a></div>
         </div>
     </div>
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -2,15 +2,14 @@
     <x-slot:title>
         {{ $title }}
     </x-slot:title>
-    <div class=" grid col-span-1 row-span-1 place-content-start">
+    <div class=" grid col-span-1 row-span-1 place-content-start max-sm:place-content-center max-lg:pt-20 max-lg:place-content-center">
         <h1 class="text-3xl text-center ">The Blogger Registration </h1>
     </div>
-    <div class=" grid grid-cols-8 h-auto max-lg:grid-cols-1 max-lg:order-last max-sm:grid-cols-12">
-
-        <div class=" grid grid-cols-1 col-span-4 place-content-center max-sm:col-span-12">
+    <div class=" grid grid-cols-8 h-auto max-lg:grid-cols-1 max-lg:order-last max-sm:grid-cols-12 max-lg:pb-10">
+        <div class=" grid grid-cols-1 col-span-4 place-content-center max-sm:col-span-12 px-10 max-md:pb-3">
             <x-register-form/>
         </div>
-        <div class="grid col-span-4  place-items-center max-lg:order-first max-sm:col-span-12">
+        <div class="grid col-span-4 place-items-center max-lg:order-first max-md:col-span-12 ">
             <img src="{{ Vite::asset('resources/images/register.webp')  }}" class="h-auto w-full" alt="registration">
         </div>
     </div>
