a89cde3264891a11b2e5ecd4540a4040f510c1d5
mina <minaremon1986@gmail.com>
2023-11-02 19:54:45 +0200

    make changes at the screen width of the registration form at the create.blade.php and the register-form.blade.php ```

--- resources/views/components/register-form.blade.php
+++ resources/views/components/register-form.blade.php
@@ -28,7 +28,6 @@
         </div>
 
     </div>
-
     <div class="grid md:grid-cols-2 md:gap-6">
         <div class="relative z-0 w-full mb-6 group">
             <input type="text" name="username" id="username"
@@ -67,7 +66,6 @@
                 class="font-medium">{{$message}}</p>
         @enderror
     </div>
-
     <div class="relative z-0 w-full mb-6 group">
         <input type="password" name="password" id="password"
                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -79,7 +77,6 @@
                 class="font-medium">{{$message}}</p>
         @enderror
     </div>
-
     <div class="relative z-0 w-full mb-6 group">
         <input type="password" name="password_confirmation" id="password_confirmation"
                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -5,13 +5,13 @@
     <div class=" grid col-span-1 row-span-1 place-content-start">
         <h1 class="text-3xl text-center ">The Blogger Registration </h1>
     </div>
-    <div class=" grid grid-cols-8 h-[67.3vh] max-lg:grid-cols-1 max-lg:h-[99.3vh] max-lg:order-last">
+    <div class=" grid grid-cols-8 h-auto max-lg:grid-cols-1 max-lg:order-last max-sm:grid-cols-12">
 
-        <div class=" grid grid-cols-1 col-span-4 place-content-center">
+        <div class=" grid grid-cols-1 col-span-4 place-content-center max-sm:col-span-12">
             <x-register-form/>
         </div>
-        <div class="grid col-span-4 place-items-center max-lg:order-first">
-            <img src="{{ Vite::asset('resources/images/register.webp')  }}" class="h-auto max-w-[50%]" alt="">
+        <div class="grid col-span-4  place-items-center max-lg:order-first max-sm:col-span-12">
+            <img src="{{ Vite::asset('resources/images/register.webp')  }}" class="h-auto w-full" alt="registration">
         </div>
     </div>
 
