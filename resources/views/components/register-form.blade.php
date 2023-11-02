0c73622fbd459c89bcd519ced12a4046bb1ac611
mina <minaremon1986@gmail.com>
2023-11-03 01:45:02 +0200

    changed some styles in the create.blade.php create.blade.php home-layout.blade.php and register-form.blade.php for fiffrent screen size

--- resources/views/components/register-form.blade.php
+++ resources/views/components/register-form.blade.php
@@ -2,7 +2,7 @@
 <form action="{{ route('register.store') }}" method="post">
     @csrf
     <div class="grid md:grid-cols-2 md:gap-6">
-        <div class="relative z-0 w-full mb-6 group row-span-2">
+        <div class="relative z-0 mb-6 w-full group row-span-2">
             <input type="text" name="first_name" id="first_name"
                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                    placeholder=" " value="{{ old('first_name') }}"/>
@@ -14,7 +14,7 @@
                     class="font-medium">{{$message}}</p>
             @enderror
         </div>
-        <div class="relative z-0 w-full mb-6 group">
+        <div class="relative z-0 mb-6 w-full group ">
             <input type="text" name="last_name" id="last_name"
                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                    placeholder=" " value="{{ old('last_name') }}"/>
@@ -41,8 +41,7 @@
                     class="font-medium">{{$message}}</p>
             @enderror
         </div>
-
-        <div class="relative z-0 w-full mb-6 group">
+        <div class="relative z-0 w-full mb-6 group ">
             <input type="text" name="phone" id="phone"
                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                    placeholder=" " value="{{ old('phone') }}"/>
@@ -77,7 +76,7 @@
                 class="font-medium">{{$message}}</p>
         @enderror
     </div>
-    <div class="relative z-0 w-full mb-6 group">
+    <div class="relative z-0 w-full mb-6 group 2">
         <input type="password" name="password_confirmation" id="password_confirmation"
                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                placeholder="" value=""/>
@@ -89,9 +88,9 @@
                 class="font-medium">{{$message}}</p>
         @enderror
     </div>
-    <div class="grid place-items-start w-full max-sm:grid-cols-1 max-sm:place-items-center">
+    <div class="grid grid-cols-2 place-items-start mb-6 w-full max-sm:grid-cols-1 max-sm:place-items-center 2xl:mt-12">
         <button type="submit"
-                class="grid place-items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                class="grid  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Create account
         </button>
         <div class="grid place-content-center place-items-center w-full text-sm font-medium text-gray-500 dark:text-gray-300 max-sm:pt-2 max-lg:place-content-start max-lg:mt-3">
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -2,15 +2,30 @@
     <x-slot:title>
         {{ $title }}
     </x-slot:title>
-    <div class=" grid col-span-1 row-span-1 place-content-start max-sm:place-content-center max-lg:pt-20 max-lg:place-content-center">
-        <h1 class="text-3xl text-center ">The Blogger Registration </h1>
-    </div>
-    <div class=" grid grid-cols-8 h-auto max-lg:grid-cols-1 max-lg:order-last max-sm:grid-cols-12 max-lg:pb-10">
-        <div class=" grid grid-cols-1 col-span-4 place-content-center max-sm:col-span-12 px-10 max-md:pb-3">
+    <div class="grid grid-cols-8 h-auto max-lg:grid-cols-1 max-lg:order-last max-sm:grid-cols-12 max-lg:pb-10 lg:grid-cols-12">
+        <div class="grid grid-cols-1 col-span-4 place-content-center max-sm:col-span-12 px-10 max-md:pb-3 lg:col-span-6 lg:mt-5 lg:mb-5 xl:mt-28 sm:mt-8">
+            <h1 class="text-3xl text-center max-sm:text-sm">The Blogger Registration </h1>
+            <figure class="max-w-screen-md mx-auto text-center mb-3">
+                <svg class="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                    <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+                </svg>
+                <blockquote>
+                    <p class="text-2xl italic font-medium text-gray-900 dark:text-white max-sm:text-sm lg:text-sm xl:text-lg 2xl:text-2xl">"An artist is someone who uses bravery, insight, creativity, and boldness to challenge the status quo. And an artist takes it personally."</p>
+                </blockquote>
+                <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                    <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture">
+                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                        <cite class="pr-3 font-medium text-gray-900 dark:text-white max-sm:text-sm">Seth Godin</cite>
+                        <cite class="pl-3 text-sm text-gray-500 dark:text-gray-400">
+                            American author
+                        </cite>
+                    </div>
+                </figcaption>
+            </figure>
             <x-register-form/>
         </div>
-        <div class="grid col-span-4 place-items-center max-lg:order-first max-md:col-span-12 ">
-            <img src="{{ Vite::asset('resources/images/register.webp')  }}" class="h-auto w-full" alt="registration">
+        <div class="grid col-span-4 place-items-center bg-gray-100 max-lg:order-first max-md:col-span-12 lg:col-span-6 ">
+            <img src="{{ Vite::asset('resources/images/register.png')  }}" class="h-auto w-full" alt="registration">
         </div>
     </div>
 
