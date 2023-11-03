f157d1441bc997210ac4153e12d6bcef821dfd16
mina <minaremon1986@gmail.com>
2023-11-03 11:29:42 +0200

    make style changes at the create.blade.php

--- resources/views/login/create.blade.php
+++ resources/views/login/create.blade.php
@@ -10,7 +10,7 @@
 
     <div class="grid grid-cols-1 place-content-center place-items-center max-sm:p-20 sm:p-20 md:p-28 lg:p-28 xl:p-3.5">
 
-        <h1 class="place-content-start font-extrabold">Sign in to your account</h1>
+        <h1 class="place-content-start font-extrabold">Sign in to your account </h1>
         <x-login-form/>
     </div>
 
