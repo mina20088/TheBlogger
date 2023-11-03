2c2d82f9e7a608a145ade1a7f144968558b1b9c5
mina <minaremon1986@gmail.com>
2023-11-03 11:30:59 +0200

    Revert "Revert "make style changes at the create.blade.php""
    This reverts commit f1e4724048ae2265cdce95536c83079bb1e0fe97.

--- resources/views/login/create.blade.php
+++ resources/views/login/create.blade.php
@@ -8,7 +8,7 @@
 
     @include('components.partials._authentication_fail_rate_limter_error')
 
-    <div class="grid grid-cols-1 place-content-center place-items-center max-sm:p-20 sm:p-20 md:p-28 lg:p-28 xl:p-3.5">
+    <div class="grid grid-cols-1 place-content-center place-items-center max-sm:p-20 sm:p-20 md:p-28 lg:p-28 xl:p-56">
 
         <h1 class="place-content-start font-extrabold">Sign in to your account </h1>
         <x-login-form/>
