932f9e49d002eb364ac5cac2371054976888bfbd
mina <minaremon1986@gmail.com>
2023-11-05 17:43:38 +0200

    changed the auth.php `'throttle' => 0` to        'throttle' => 60` removed this line `{{--<img class="sm-h-8" src="{{ asset('logo.png') }}" alt style="max-width: 100%; vertical-align: middle; line-height: 1; border: 0; height: auto; width: 256px">--}}` make changes to the EventServiceProvider.php added a to the PasswordRestController.php ```     public function store(ResetPasswordFormRequest $request)     {          $status = \Password::sendResetLink(['email'=> $request->only('email')]);
             dd($status);
        }
    ```
    added the below code to the RegisterController.php
    ```
            event(new Registered($user));
    
            $user->notify(new SendWelcomeEmailNotification());
    
    ```
    
    removed line from the RegistrationWelcomeEmail.php
    created the reset_password.blade.php
    created the SendWelcomeEmailNotification.php class to sed welcome emails to the user
    add the CanResetPassword interface at the User.php
    added the route to the web.php
    ```
    Route::get('/reset-password/{token}', function (string $token) {
    
    })->middleware('guest')->name('password.reset');
    ```
    
    `

---
+++ app/Notifications/SendRestEmailNotification.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Mail\RegistrationWelcomeEmail;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class SendRestEmailNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function     __construct(
+        protected  string $token
+    ){}
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(User $user):MailMessage
+    {
+        return(new MailMessage())
+            ->subject('Reset Password Email')
+            ->view('emails.reset_password',['token'=> $this->token]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
