530a8fbbf9a9ab5c62d8da844b4f447b2ce62e16
minaremon <minaremonshaker@gmail.com>
2024-01-06 12:44:00 +0200

    modified:   .timetracker 	modified:   database/migrations/2024_01_06_103322_create_old_password_table.php

--- database/migrations/2024_01_06_103322_create_old_password_table.php
+++ database/migrations/2024_01_06_103322_create_old_password_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('old_password', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
