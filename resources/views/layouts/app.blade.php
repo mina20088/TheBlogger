47e4f61d722dea01e2c0b7b782c5ab7fd8b23a19
mina remon <minaremonshaker@gmail.com>
2024-01-19 01:33:03 +0200

    Changes to be committed: 	modified:   .timetracker 	modified:   resources/css/app.css 	modified:   resources/js/app.js 	modified:   resources/views/layouts/app.blade.php 	modified:   resources/views/partials/__dashboard-header.blade.php 	modified:   resources/views/partials/__dashboard-sidebar.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,10 @@
 <!doctype html>
-<!--
-* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
-* @version 1.0.0-beta19
-* @link https://tabler.io
-* Copyright 2018-2023 The Tabler Authors
-* Copyright 2018-2023 codecalm.net Paweł Kuna
-* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
--->
 <html lang="en">
   <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Dashboard - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+    <title>Dashboard</title>
     <!-- CSS files -->
     <link href="./dist/css/tabler.min.css?1684106062" rel="stylesheet"/>
     <link href="./dist/css/tabler-flags.min.css?1684106062" rel="stylesheet"/>
@@ -28,6 +20,7 @@
       	font-feature-settings: "cv03", "cv04", "cv11";
       }
     </style>
+     
   </head>
   <body  class=" layout-fluid">
     <script src="./dist/js/demo-theme.min.js?1684106062"></script>
