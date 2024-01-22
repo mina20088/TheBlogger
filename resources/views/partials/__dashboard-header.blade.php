fbddccddde57eb6bfb153cb25e258e9cb2e0d45c
minaremon <minaremonshaker@gmail.com>
2024-01-22 19:03:45 +0200

    Changes to be committed: 	modified:   .gitignore 	modified:   .timetracker 	new file:   app/View/Components/dashboard-user-dropdown.php 	modified:   resources/views/partials/__dashboard-header.blade.php 	modified:   resources/views/partials/__dashboard-sidebar.blade.php 	new file:   resources/views/partials/__dashboard-user-dropdown.blade.php

--- resources/views/partials/__dashboard-header.blade.php
+++ resources/views/partials/__dashboard-header.blade.php
@@ -7,23 +7,7 @@
           @include('partials.__dashboard-light-mode-toggler-button')
           @include('partials.__dashboard-notification-dropdown')
         </div>
-        <div class="nav-item dropdown">
-          <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-            <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
-            <div class="d-none d-xl-block ps-2">
-              <div>Paweł Kuna</div>
-              <div class="mt-1 small text-muted">UI Designer</div>
-            </div>
-          </a>
-          <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-            <a href="#" class="dropdown-item">Status</a>
-            <a href="./profile.html" class="dropdown-item">Profile</a>
-            <a href="#" class="dropdown-item">Feedback</a>
-            <div class="dropdown-divider"></div>
-            <a href="./settings.html" class="dropdown-item">Settings</a>
-            <a href="./sign-in.html" class="dropdown-item">Logout</a>
-          </div>
-        </div>
+        @include('partials.__dashboard-user-dropdown')
       </div>
     </div>
   </header>
--- resources/views/partials/__dashboard-sidebar.blade.php
+++ resources/views/partials/__dashboard-sidebar.blade.php
@@ -10,7 +10,7 @@
           @include('partials.__dashboard-dark-mode-toggler-button')
           @include('partials.__dashboard-light-mode-toggler-button')
           @include('partials.__dashboard-notification-dropdown')
-          <div class="nav-item dropdown d-none d-md-flex me-3">
+          {{-- <div class="nav-item dropdown d-none d-md-flex me-3">
             <a href="#" class="nav-link px-0" data-bs-toggle="dropdown" tabindex="-1" aria-label="Show notifications">
               <!-- Download SVG icon from http://tabler-icons.io/i/bell -->
               <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /></svg>
@@ -93,26 +93,9 @@
                 </div>
               </div>
             </div>
-          </div>
+          </div> --}}
         </div>
-        
-        <div class="nav-item dropdown"> 
-          <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-            <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
-            <div class="d-none d-xl-block ps-2">
-              <div>Paweł Kuna</div>
-              <div class="mt-1 small text-muted">UI Designer</div>
-            </div>
-          </a>
-          <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-            <a href="#" class="dropdown-item">Status</a>
-            <a href="./profile.html" class="dropdown-item">Profile</a>
-            <a href="#" class="dropdown-item">Feedback</a>
-            <div class="dropdown-divider"></div>
-            <a href="./settings.html" class="dropdown-item">Settings</a>
-            <a href="./sign-in.html" class="dropdown-item">Logout</a>
-          </div>
-        </div>
+          @include('partials.__dashboard-user-dropdown')
       </div>
       <x-dashboard_side_bar_menu>
         <x-dashboard_side_bar_menu_wrapper>
