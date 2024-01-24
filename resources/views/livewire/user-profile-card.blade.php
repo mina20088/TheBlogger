4900057f29a613323cbd2cdfe02c56cf19e622f0
minaremon <minaremonshaker@gmail.com>
2024-01-24 19:39:19 +0200

    Changes to be committed: 	modified:   .timetracker 	modified:   resources/css/dashboard.css 	modified:   resources/views/layouts/app.blade.php 	modified:   resources/views/livewire/user-profile-card.blade.php

--- resources/views/livewire/user-profile-card.blade.php
+++ resources/views/livewire/user-profile-card.blade.php
@@ -1,18 +1,107 @@
-<div class="card">
-    <div class="card-body p-4 text-center">
-      <img src="{{ Vite::asset('resources/images/avatars/000m.jpg')}}" class="avatar avatar-xl mb-3 rounded"/>
-      <h3 class="m-0 mb-1"><a href="#"> {{  $firstName . " "  . $lastName }}</a></h3>
-      <div class="text-muted">UI Designer</div>
-      <div class="mt-3">
-        <span class="badge bg-purple-lt">Owner</span>
-      </div>
-    </div>
-    <div class="d-flex">
-      <a href="#" class="card-btn"><!-- Download SVG icon from http://tabler-icons.io/i/mail -->
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path><path d="M3 7l9 6l9 -6"></path></svg>
-        Email</a>
-      <a href="#" class="card-btn"><!-- Download SVG icon from http://tabler-icons.io/i/phone -->
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path></svg>
-        follow</a>
+@pushOnce('styles')
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+@endpushOnce
+
+
+<div
+  class="z-50 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <div class="flex justify-end px-4 pt-4">
+    <button id="dropdownButton" data-dropdown-toggle="dropdown"
+      class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+      type="button">
+      <span class="sr-only">Open dropdown</span>
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+        <path
+          d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+      </svg>
+    </button>
+    <!-- Dropdown menu -->
+    <div id="dropdown"
+      class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+      <ul class="py-2" aria-labelledby="dropdownButton">
+        <li>
+          <a href="#"
+            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
+        </li>
+        <li>
+          <a href="#"
+            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Export
+            Data</a>
+        </li>
+        <li>
+          <a href="#"
+            class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+        </li>
+      </ul>
     </div>
   </div>
+  <div class="flex flex-col items-center pb-10">
+    <div class="relative">
+      <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ Vite::asset('resources/images/avatars/000m.jpg') }}"
+        alt="Bonnie image" />
+
+      <button class="absolute top-14 right-0" data-modal-target="popup-modal" data-modal-toggle="popup-modal"
+        data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="upload profile picture" href="#">
+        <svg class=" w-5 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor" viewBox="0 0 24 24">
+          <path fill-rule="evenodd"
+            d="M12 3c.3 0 .6.1.8.4l4 5a1 1 0 1 1-1.6 1.2L13 7v7a1 1 0 1 1-2 0V6.9L8.8 9.6a1 1 0 1 1-1.6-1.2l4-5c.2-.3.5-.4.8-.4ZM9 14v-1H5a2 2 0 0 0-2 2v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-4v1a3 3 0 1 1-6 0Zm8 2a1 1 0 1 0 0 2 1 1 0 1 0 0-2Z"
+            clip-rule="evenodd" />
+        </svg>
+      </button>
+
+    </div>
+
+    <div id="popup-modal" tabindex="-1" aria-hidden="true"
+      class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      <div class="relative w-full max-w-4xl max-h-full">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          {{-- modal heder --}}
+          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+              upload Profile Picture
+            </h3>
+            <button type="button"
+              class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-hide="popup-modal">
+              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+          </div>
+          <!-- Modal body -->
+          <div class="p-4 md:p-5">
+            <form wire:submit.prevent="upload">
+              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload
+                file</label>
+              <input
+                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-600 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 file:bg-blue-500"
+                id="file_input" type="file" />
+                <button type="submit" type="button" class="px-5 py-2.5 mt-5 w-full text-sm font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">upload</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ $firstName . " " . $lastName }}</h5>
+    <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+    <div class="flex mt-4 md:mt-6">
+      <a href="#"
+        class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
+        friend</a>
+      <a href="#"
+        class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Message</a>
+    </div>
+  </div>
+</div>
+
+@pushOnce('scripts')
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+@endPushOnce
